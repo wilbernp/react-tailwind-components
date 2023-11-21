88256e63a70647d6b2a2b1a58b90cb5381709fd1
wilbernp <wilbernarvaezpetro@gmail.com>
2023-11-21 20:08:37 +0800

    add input commponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Input } from './components/Input';
+
 export default function App() {
-  return <div className="w-52 bg-gray-400">app</div>;
+  return (
+    <div className="space-y-2">
+      <h3>inputs</h3>
+      <Input placeholder="Input sm" size="sm" fullWidth />
+      <Input placeholder="Input md" size="md" />
+      <Input placeholder="Input lg" size="lg" />
+    </div>
+  );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,64 @@
-export default function Input() {
-  return <div></div>;
-}
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/utils';
+
+const inputVariants = cva(
+  'border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block focus:ring-1',
+  {
+    variants: {
+      size: {
+        sm: 'px-2 sm:text-xs h-8',
+        md: 'px-2.5 h-10',
+        lg: 'px-4 sm:text-md h-14',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    },
+  },
+);
+
+type InputPropsBase = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>;
+
+type InputProps = InputPropsBase &
+  VariantProps<typeof inputVariants> & {
+    fullWidth?: boolean;
+    startAdornment?: JSX.Element;
+    endAdornment?: JSX.Element;
+  };
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      type,
+      size,
+      fullWidth,
+      startAdornment,
+      endAdornment,
+      ...props
+    },
+    ref,
+  ) => {
+    const cnButton = cn({ 'w-full': !!fullWidth });
+    return (
+      <div className="relative">
+        {
+          <div className="pointer-events-none absolute inset-y-0 left-2 my-auto flex h-10 w-10 items-center justify-center rounded-full hover:bg-gray-400">
+            <div className="h-6 w-6 rounded-full bg-gray-700"></div>
+          </div>
+        }
+        <input
+          type={type}
+          className={cn(inputVariants({ size, className }), cnButton)}
+          ref={ref}
+          {...props}
+        />
+        {startAdornment && <div>{startAdornment}</div>}
+      </div>
+    );
+  },
+);
+Input.displayName = 'Input';
+
+export { Input };
